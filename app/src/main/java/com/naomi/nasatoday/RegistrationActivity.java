bba9d41e28f5ad876c8df788b309785978a7f745
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-05-12 17:38:11 +0300

    added onclickListener for login and text view

--- app/src/main/java/com/naomi/nasatoday/RegistrationActivity.java
+++ app/src/main/java/com/naomi/nasatoday/RegistrationActivity.java
@@ -1,7 +1,11 @@
 package com.naomi.nasatoday;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        TextView login = (TextView)findViewById(R.id.lnkLogin);
+        login.setMovementMethod(LinkMovementMethod.getInstance());
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                Intent intent = new Intent (RegistrationActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
