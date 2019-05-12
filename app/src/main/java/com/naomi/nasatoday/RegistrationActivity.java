92f9d26bbba49f03a13893d9993da9856ee88ff7
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-05-12 23:54:21 +0300

    added firebase to registration activity and on click method

--- app/src/main/java/com/naomi/nasatoday/RegistrationActivity.java
+++ app/src/main/java/com/naomi/nasatoday/RegistrationActivity.java
@@ -1,20 +1,37 @@
 package com.naomi.nasatoday;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
+    private EditText FullName, Email, Password;
+    private Button Login;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        auth = FirebaseAuth.getInstance();
+        Login = (Button) findViewById(R.id.btnLogin);
+        FullName = (EditText) findViewById(R.id.txtName);
+        Email = (EditText) findViewById(R.id.txtEmail);
+        Password = (EditText) findViewById(R.id.txtPwd);
         TextView login = (TextView)findViewById(R.id.lnkLogin);
         login.setMovementMethod(LinkMovementMethod.getInstance());
         login.setOnClickListener(new View.OnClickListener() {
@@ -24,11 +41,34 @@ public class RegistrationActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
 
-    public void activateLogin(View view) {
-        //when login button is clicked.
-        Log.i("tag", "activateLogin: Please write your username and password");
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = Email.getText().toString().trim();
+                String password = Password.getText().toString().trim();
+                if (TextUtils.isEmpty((CharSequence) FullName)) {
+                    Toast.makeText(getApplicationContext(), "Enter name!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(RegistrationActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
 
+            }
+        }
     }
 }
