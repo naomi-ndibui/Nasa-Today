3b3e7413231f0b0259ef47c312c77c26cef5f45a
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-05-12 18:58:45 +0300

    added view oncreateview to view places and location and arrayadpter and onlistitemclick to display the place and location

--- app/src/main/java/com/naomi/nasatoday/ListMenu.java
+++ app/src/main/java/com/naomi/nasatoday/ListMenu.java
@@ -1,13 +1,29 @@
 package com.naomi.nasatoday;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ListFragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class ListMenu extends AppCompatActivity {
+public class ListMenu extends ListFragment {
+    String[] places = new String[] { "Earth","Stars","Planets"};
+    String[] location = new String[]{"Space view","Space view","Space view"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_menu);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.activity_list_menu, container, false);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, places);
+        setListAdapter(adapter);
+        return view;
+    }
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Details txt = (Details)getFragmentManager().findFragmentById(R.id.fragment2);
+        txt.change("Name: "+ places[position],"Location : "+ location[position]);
+        getListView().setSelector(android.R.color.holo_blue_dark);
     }
 }
