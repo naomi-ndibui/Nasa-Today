f7a9bcff0540ea8f3fa8508895b552c26e28dcb9
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-05-12 17:30:57 +0300

    added onclicklistener for registration and text view

--- app/src/main/java/com/naomi/nasatoday/HomeActivity.java
+++ app/src/main/java/com/naomi/nasatoday/HomeActivity.java
@@ -1,7 +1,11 @@
 package com.naomi.nasatoday;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        TextView register = (TextView)findViewById(R.id.lnkRegister);
+        register.setMovementMethod(LinkMovementMethod.getInstance());
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V){
+                Intent intent = new Intent(HomeActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
