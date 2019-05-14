d82f98908bb281e104861803700519db0ecbeb3a
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-05-14 11:32:40 +0300

    initialized firebase

--- app/src/main/java/com/naomi/nasatoday/MyCustomApplication.java
+++ app/src/main/java/com/naomi/nasatoday/MyCustomApplication.java
@@ -1,4 +1,23 @@
 package com.naomi.nasatoday;
 
-public class MyCustomApplication {
+import android.app.Application;
+import android.content.res.Configuration;
+
+import com.google.firebase.FirebaseApp;
+
+public class MyCustomApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+                FirebaseApp.initializeApp(this);
+
+    }
+    @Override
+    public void onConfigurationChanged (Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
 }
