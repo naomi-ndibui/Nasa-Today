7990edb2ea0e707a1cd515bff63d934ed590b6d8
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-05-17 11:44:08 +0300

    created a new OkHttpClient to create and send our request. It'll tie it to consumerSignPost object responsible for creating a signature

--- app/src/main/java/Activity/NasaService.java
+++ app/src/main/java/Activity/NasaService.java
@@ -3,12 +3,24 @@ package Activity;
 import java.util.Date;
 
 import okhttp3.Callback;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 
 public class NasaService {
     public static void(Date date, Boolean hd, Callback callback) {
 
         OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.Nasa_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.Nasa_date_QUERY_PARAMETER, String.valueOf(date));
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Authorization", Constants.Nasa_Token)
                 .build();
 
     }
