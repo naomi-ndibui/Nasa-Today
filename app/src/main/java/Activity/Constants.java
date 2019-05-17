82a577b4dcc5395e2d97d16ee2ce22a95b8ca882
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-05-17 11:24:45 +0300

    added date and hd parameters

--- app/src/main/java/Activity/Constants.java
+++ app/src/main/java/Activity/Constants.java
@@ -4,4 +4,7 @@ import com.naomi.nasatoday.BuildConfig;
 
 public class Constants {
     public static final String Nasa_Token = BuildConfig.Nasa_TOKEN;
+    public static final String Nasa_BASE_URL ="https://api.nasa.gov/planetary/apod";
+    public static final String Nasa_date_QUERY_PARAMETER = "date";
+    public static final String Nasa_hd_QUERY_PARAMETER = "hd";
 }
