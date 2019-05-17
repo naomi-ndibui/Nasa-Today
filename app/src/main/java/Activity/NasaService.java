e9ad70e326b110ff8ccee5eb005c7713f778b78c
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-05-17 11:33:56 +0300

    added a new class and added date user provide and the hd

---
+++ app/src/main/java/Activity/NasaService.java
@@ -0,0 +1,11 @@
+package Activity;
+
+import java.util.Date;
+
+import okhttp3.Callback;
+
+public class NasaService {
+    public static void(Date date, Boolean hd, Callback callback) {
+
+    }
+}
