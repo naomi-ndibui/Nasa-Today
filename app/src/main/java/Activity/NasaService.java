adf58618a4f954f1675838cd3f68e1a390c290d5
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-05-17 11:35:11 +0300

    created a new OkHttpClient client to create and send our request

--- app/src/main/java/Activity/NasaService.java
+++ app/src/main/java/Activity/NasaService.java
@@ -3,9 +3,13 @@ package Activity;
 import java.util.Date;
 
 import okhttp3.Callback;
+import okhttp3.OkHttpClient;
 
 public class NasaService {
     public static void(Date date, Boolean hd, Callback callback) {
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
 
     }
 }
