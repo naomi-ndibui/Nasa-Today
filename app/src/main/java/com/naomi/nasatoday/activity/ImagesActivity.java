e8b0d31ec2af46d64c0cf2964f1c7964a20e65ea
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-05-28 22:33:39 +0300

    added grid view

--- app/src/main/java/com/naomi/nasatoday/activity/ImagesActivity.java
+++ app/src/main/java/com/naomi/nasatoday/activity/ImagesActivity.java
@@ -2,6 +2,10 @@ package com.naomi.nasatoday.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+
+import com.naomi.nasatoday.R;
+import com.naomi.nasatoday.adapter.ImageAdapter;
 
 public class ImagesActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class ImagesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_images);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
     }
 }
