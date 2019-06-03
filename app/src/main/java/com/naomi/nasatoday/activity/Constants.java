87530c6edddeec6983e28226e861486149b6711e
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-06-03 08:58:46 +0300

    defined the child name of the Firebase node to save this information to in Constants.java.

--- app/src/main/java/com/naomi/nasatoday/activity/Constants.java
+++ app/src/main/java/com/naomi/nasatoday/activity/Constants.java
@@ -6,4 +6,7 @@ public class Constants {
     public static final String Nasa_Token = BuildConfig.Nasa_TOKEN;
     public static final String Nasa_BASE_URL ="https://api.nasa.gov/planetary/apod";
     public static final String Nasa_date_QUERY_PARAMETER = "date";
+
+    public static final String FIREBASE_CHILD_SEARCHED_DATE = "searchedDATE";
+
 }
