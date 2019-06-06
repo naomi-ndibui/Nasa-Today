61e4b9b6c4ab6850ddd66acfe47041783a40fbfc
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-06-06 10:12:38 +0300

    added interfaces

---
+++ app/src/main/java/com/naomi/nasatoday/activity/Util/OnStartDragListener.java
@@ -0,0 +1,8 @@
+package com.naomi.nasatoday.activity.Util;
+
+import android.support.v7.widget.RecyclerView;
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+
+}
